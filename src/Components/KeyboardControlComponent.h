7550fe8d0408134731198a7e52a9973f0c5bc2b5
Frederic Bourdeau <d1amond456.fb@gmail.com>
2020-02-05 16:35:33 -0500

    Started keyboard input component

---
+++ src/Components/KeyboardControlComponent.h
@@ -0,0 +1,14 @@
+#ifndef KEYBOARDCONTROLCOMPONENT_H
+#define KEYBOARDCONTROLCOMPONENT_H
+
+#include "../Game.h"
+#include "../EntityManager.h"
+#include "./TransformComponent.h"
+#include "./SpriteComponent.h"
+
+class KeyboardControlComponent: public Component {
+public:
+	
+}
+
+#endif
